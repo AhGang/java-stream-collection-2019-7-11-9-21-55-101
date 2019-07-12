c8052286ecf3290c43410f6a0b595956037f9e6b
AhGang <474494594@qq.com>
2019-07-12 23:08:29 +0800

    Finish Add Test,运用filter,map，reduce，MapToInt等方法解决一些实用场景

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,11 +2,24 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Add {
+    public List<Integer>  buildList(int leftBorder, int rightBorder){
+        int start = leftBorder < rightBorder ? leftBorder : rightBorder;
+        int end =  leftBorder > rightBorder ? leftBorder: rightBorder;
+        return Stream.iterate(start, n -> n + 1).limit(end).collect(Collectors.toList());
+    }
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+
+        List<Integer> arrayList = buildList(leftBorder,rightBorder);
+        arrayList = arrayList.stream().filter(item -> item % 2 == 0 ).collect(Collectors.toList());
+        return arrayList.stream().reduce((a,b) -> a + b).get();
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
@@ -14,11 +27,11 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return  arrayList.stream().mapToInt(number -> number * 3 + 2).sum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(item ->(item % 2 == 1 ?  item * 3 + 2: item)).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
@@ -30,7 +43,9 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        arrayList = arrayList.stream().filter(item -> item % 2 == 0 ).collect(Collectors.toList());
+        return  arrayList.stream().collect(Collectors.averagingInt(item -> item));
+        // return arrayList.stream().mapToInt(i -> i).average();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
@@ -38,7 +53,9 @@ public class Add {
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        arrayList = arrayList.stream().filter(item -> item % 2 == 0 ).collect(Collectors.toList());
+        Set<Integer> result = new HashSet<>();
+        return arrayList.stream().filter(result::add).collect(Collectors.toList());
     }
 
 //    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
