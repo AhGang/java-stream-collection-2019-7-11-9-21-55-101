538008fe20f8bf19b42310f2410393e70a482c20
AhGang <474494594@qq.com>
2019-07-12 21:58:08 +0800

    Finish 关于 MyMap 的训练,主要通过Stream.map进行列表的映射以及Stream.sorted进行列表的排序

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,6 +3,7 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,7 +23,8 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+       // return array.stream().map(item -> (char)item).collect(Collectors.toList());
+        return array.stream().map(item ->letters[item-1]).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
@@ -30,10 +32,10 @@ public class MyMap {
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
