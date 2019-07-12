08b94f4506aeffafef4a0f11d18b42dfb14d198d
AhGang <474494594@qq.com>
2019-07-12 13:29:41 +0800

    Finish 关于 Filter 的训练

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,8 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +14,36 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+//        List<Integer> result = new ArrayList<>();
+//        for(int i = 0; i <array.size();i++){
+//            if(array.get(i) % 2 == 0){
+//                result.add(array.get(i));
+//            }
+//        }
+        return array.stream().filter(item -> item % 2 == 0 ).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(item -> item % 3 == 0 ).collect(Collectors.toList());
+    }
+
+
+    public List<Integer> getDifferentElements() {
+        Set<Integer> result = new HashSet<>();
+        return array.stream().filter(result::add).collect(Collectors.toList());
+//        return array.stream().collect(Collectors.toSet()).stream().collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+//        List<Integer> result = new ArrayList<>();
+//        for(int i = 0; i < firstList.size();i++){
+//            for(int j = 0;j < secondList.size();j++){
+//                if(firstList.get(i) == secondList.get(j)){
+//                    result.add(firstList.get(i));
+//                }
+//            }
+//        }
+//
+       return firstList.stream().filter(secondList::contains).collect(Collectors.toList());
     }
 }
