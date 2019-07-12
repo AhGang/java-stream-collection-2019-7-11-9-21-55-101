0f870d56e0ecd66be603a4df79ceac46a6f3b4b3
AhGang <474494594@qq.com>
2019-07-12 23:29:23 +0800

    Finish CollectionOperator Test,运用filter,map，reduce，MapToInt等方法解决一些关于集合的实用场景

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,37 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
+    public List<Integer>  buildList(int left, int right){
+        if(left > right ){
+            return Stream.iterate(left, n -> n - 1).limit(left).collect(Collectors.toList());
+        }else{
+            return Stream.iterate(left, n -> n + 1).limit(right).collect(Collectors.toList());
+        }
+
+    }
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        return buildList(left,right);
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        return buildList(left,right).stream().filter(item -> item % 2 == 0 ).collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return IntStream.of(array).boxed().filter(i -> i % 2 == 0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length-1];
+
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
